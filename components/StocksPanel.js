1b58a0e66e92d435fddc97d5214e780897db4374
bhupendra1011 <bhupendra1011@gmail.com>
2020-01-09 19:46:24 +0530

    added stocks panel

--- components/StocksPanel.js
+++ components/StocksPanel.js
@@ -1,73 +1,127 @@
 import React from "react";
 import {
-  asset,
-  AppRegistry,
-  Environment,
-  StyleSheet,
-  Text,
-  View,
-  VrButton
+	asset,
+	AppRegistry,
+	Environment,
+	StyleSheet,
+	Text,
+	View,
+	VrButton
 } from "react-360";
 
 export default class StocksPanel extends React.Component {
-  state = {};
+	state = {};
 
-  handleStockSelection() {}
+	handleStockSelection() {}
 
-  render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <VrButton onClick={() => this.handleStockSelection()}>
-            <View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
-              <Text> INFY </Text>
-              <Text> 10% </Text>
-            </View>
-          </VrButton>
-          <VrButton onClick={() => this.handleStockSelection()}>
-            <View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
-              <Text> SBI </Text>
-              <Text> 5% </Text>
-            </View>
-          </VrButton>
-        </View>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.panel}>
+				<Text style={{ textAlign: "center", fontWeight: "bold" }}>
+					MUST HAVE STOCKS
+				</Text>
+				<View style={styles.greetingBox}>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text> ONGC </Text>
+						<Text> 125.10 </Text>
+					</View>
+
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text> LT </Text>
+						<Text> 1316.5 </Text>
+					</View>
+
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text> TITAN </Text>
+						<Text> 1163.45 </Text>
+					</View>
+
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>ICICI BANK</Text>
+						<Text>547</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>HCL</Text>
+						<Text>580</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>NTPC</Text>
+						<Text>119.35</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>WIPRO</Text>
+						<Text>256.50</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>GAIL</Text>
+						<Text>125.40</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>TATA MOTORS</Text>
+						<Text>192.75</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>YES BANK</Text>
+						<Text>48.5</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>SBI</Text>
+						<Text>331.40</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>JSW STEEL</Text>
+						<Text>279.20</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>HDFC BANK</Text>
+						<Text>1275.80</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>ASIAN PAINTS</Text>
+						<Text>745</Text>
+					</View>
+					<View style={[styles.stockBox, styles.alignCenter, styles.gainers]}>
+						<Text>ITC</Text>
+						<Text>236.50</Text>
+					</View>
+				</View>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: "rgba(255, 255, 255, 0.4)",
-    flexDirection: "row",
-    justifyContent: "space-between"
-  },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: "#000000",
-    borderColor: "#639dda",
-    borderWidth: 2,
-    flexDirection: "row",
-    display: "flex",
-    width: "100%"
-  },
-  gainers: {
-    backgroundColor: "#509d0a"
-  },
-  losers: {
-    backgroundColor: "#d22222f7"
-  },
-  alignCenter: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  stockBox: {
-    width: 80,
-    height: 80,
-    margin: 10
-  }
+	panel: {
+		// Fill the entire surface
+		width: 350,
+		height: 550,
+		backgroundColor: "rgba(255, 255, 255, 0.4)"
+	},
+	greetingBox: {
+		width: 350,
+		backgroundColor: "#000000",
+		borderColor: "#639dda",
+		borderWidth: 2,
+
+		flex: 1,
+		flexWrap: "wrap",
+		flexDirection: "row",
+		justifyContent: "space-between"
+	},
+	gainers: {
+		backgroundColor: "#0955a2db"
+	},
+	losers: {
+		backgroundColor: "#d22222f7"
+	},
+	alignCenter: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	stockBox: {
+		width: 80,
+		height: 80,
+		margin: 10
+	}
 });
